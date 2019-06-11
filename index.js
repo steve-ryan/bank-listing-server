9c6a09c6bc928718ef2cf413a6c3e01691bd38be
steve <stevohyoung@gmail.com>
2019-06-11 12:11:01 +0300

    add/api/bank route

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
-const name = "steve";
+const greeting = "Hello Steve!!";
+const pool = mysql.createPool({
 
-app.get("/",function(req, res){
-    //logic
-    res.json({message:"Hello steve"});
+    host: "localhost",
+    user: "root",
+    password: "Rozah1997",
+    database: "bank_listing"
 });
 
-app.listen(9000, function(){
+app.get("/", (req, res) => {
+
+    //res.send("Hello steve" );
+    res.json({ message: greeting + " This is awesome, the server is up!!" });
+});
+
+app.get("/api/bank", (req, res) => {
+    //fetch bank
+    pool.query("SELECT * FROM Bank", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+        }
+        res.json(rows);
+    });
+});
+
+app.listen(9000, function () {
     console.log("App listening on port 9000");
 });
