2a31e02820c8390b17727cb00a85b64b25bd31f2
steve <stevohyoung@gmail.com>
2019-06-13 22:51:22 +0300

    added end point apis

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
-const greeting = "Hello Steve!!";
 const pool = mysql.createPool({
 
     host: process.env.DB_HOST,
@@ -11,15 +10,37 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME
 });
 
-app.get("/", (req, res) => {
-
-    //res.send("Hello steve" );
-    res.json({ message: greeting + " This is awesome, the server is up!!" });
-});
-
 app.get("/api/bank", (req, res) => {
     //fetch bank
     pool.query("SELECT * FROM Bank", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+        }
+        res.json(rows);
+    });
+});
+
+app.get("/api/category/local", (req, res) => {
+    pool.query("SELECT Logo_url,Bank_name,Count(DISTINCT Bank_id) as number_of_branches FROM Bank WHERE category='local'", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+        }
+        res.json(rows);
+    });
+});
+
+app.get("/api/category/international", (req, res) => {
+    pool.query("SELECT Logo_url,Bank_name,Count(DISTINCT Bank_id) as number_of_branches FROM Bank  WHERE category='international'", (error, rows) => {
+        if (error) {
+            return res.status(500).json({ error });
+        }
+        res.json(rows);
+    });
+});
+
+
+app.get("/api/branch", (req, res) => {
+    pool.query("SELECT b.Logo_url, br.Branch_name,br.Location,br.Email, br.Operation_hrs FROM Branch as br JOIN Bank as b ON br.Bank_id = b.Bank_id;", (error, rows) => {
         if (error) {
             return res.status(500).json({ error });
         }
