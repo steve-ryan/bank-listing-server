843663b4997c7db22b9782c5f3db0845fc87fc92
steve <stevohyoung@gmail.com>
2019-06-25 12:59:07 +0300

    added client

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mysql = require("mysql");
 const app = express();
 const pool = mysql.createPool({
-
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -24,66 +23,94 @@ app.get("/api/bank", (req, res) => {
 });
 
 app.get("/api/category/local", (req, res) => {
-    pool.query("SELECT Logo_url,Bank_name,Count(DISTINCT Bank_id) as number_of_branches FROM Bank WHERE category='local'", (error, rows) => {
-        if (error) {
-            return res.status(500).json({ error });
+    pool.query(
+        "SELECT Logo_url,Bank_name,Count(DISTINCT Bank_id) as number_of_branches FROM Bank WHERE category='local'",
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+            res.json(rows);
         }
-        res.json(rows);
-    });
+    );
 });
 
 app.get("/api/category/international", (req, res) => {
-    pool.query("SELECT Logo_url,Bank_name,Count(Bank_id) as number_in_this_category FROM Bank  WHERE category='international'", (error, rows) => {
-        if (error) {
-            return res.status(500).json({ error });
+    pool.query(
+        "SELECT Logo_url,Bank_name,Count(Bank_id) as number_in_this_category FROM Bank  WHERE category='international'",
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+            res.json(rows);
         }
-        res.json(rows);
-    });
+    );
 });
 
-
 app.get("/api/branch", (req, res) => {
-    pool.query("SELECT b.Logo_url, br.Branch_name,br.Location,br.Email, br.Operation_hrs FROM Branch as br JOIN Bank as b ON br.Bank_id = b.Bank_id;", (error, rows) => {
-        if (error) {
-            return res.status(500).json({ error });
+    pool.query(
+        "SELECT b.Logo_url, br.Branch_name,br.Location,br.Email, br.Operation_hrs FROM Branch as br JOIN Bank as b ON br.Bank_id = b.Bank_id;",
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+            res.json(rows);
         }
-        res.json(rows);
-    });
+    );
 });
 
 app.get("/api/equity/branches", (req, res) => {
-    pool.query("SELECT b.Bank_name, b.Logo_url, Location, Branch_name, Phone_Number, Email,Operation_hrs FROM Branch as br JOIN Bank as b ON  b.Bank_id=br.Bank_id AND b.Bank_id='1';", (error, rows) => {
-        if (error) {
-            return res.status(500).json({ error });
+    pool.query(
+        "SELECT b.Bank_name, b.Logo_url, Location, Branch_name, Phone_Number, Email,Operation_hrs FROM Branch as br JOIN Bank as b ON  b.Bank_id=br.Bank_id AND b.Bank_id='1';",
+        (error, rows) => {
+            if (error) {
+                return res.status(500).json({ error });
+            }
+            res.json(rows);
         }
-        res.json(rows);
-    });
+    );
 });
 //adding a bank
 app.post("/api/bank", (req, res) => {
-    const{
-        Bank_name,
-        Logo_url,
-        Category
-    }= req.body;
+    const { Bank_name, Logo_url, Category } = req.body;
 
-    if (Bank_name === "" ||Logo_url ==="" ||Category==="") {
+    if (Bank_name === "" || Logo_url === "" || Category === "") {
         return res.status(400).json({ error: "Invalid payload" });
     }
     pool.query(
         "INSERT INTO Bank (Bank_name,Logo_url,Category) VALUES (?,?,?)",
-        [Bank_name,Logo_url,Category],
+        [Bank_name, Logo_url, Category],
         (error, results) => {
             if (error) {
                 return res.status(500).json({ error });
             }
             res.json(results.insertId);
         }
-    )
-
+    );
 });
 
+/*app.put("/api/bank/:id", (req, res) => {
+    const { Bank_name} = req.body;
+    if (!Bank_name) {
+        return res.status(400).json({ error: "Invalid payload" });
+    }
 
-app.listen(9000, function () {
+    pool.query(
+        "UPDATE Bank SET Bank_name =? WHERE id = ?",
+        [Bank_name, req.params.id],
+        (error, results) => {
+            if (error) {
+                return res.json(500).json({ error });
+            }
+            res.json(results.changedRows);
+        }
+    );
+});
+app.delete("/api/bank", (req, res) => {
+    pool.query(
+        ""
+    );
+});*/
+
+app.listen(9000, () => {
     console.log("App listening on port 9000");
 });
