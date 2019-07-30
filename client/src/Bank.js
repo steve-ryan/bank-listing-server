4a5e1f6f93020d75d4c37be5f986bb1c071f00a9
stephen wachira <stevohyoung@gmail.com>
2019-07-30 15:21:50 +0300

    worked on category tab

--- client/src/Bank.js
+++ client/src/Bank.js
@@ -1,19 +1,18 @@
 import React from "react";
 import { Link } from "@reach/router";
 function Bank({ bankDisplay }) {
-    const { Bank_id, Bank_name, Logo_url } = bankDisplay;
-    return (
-        <div>
-            <img className="" src={Logo_url} alt={Bank_name} />
-            <div>
-                <div>{Bank_name}</div>
-            </div>
-            <div>
-                <Link to={`/bank/${Bank_id}`}>See Branches</Link>
-            </div>
+  const { Bank_id, Bank_name, Logo_url } = bankDisplay;
+  return (
+    <div className="bank-container">
+      <img className="banklogo" src={Logo_url} alt={Bank_name} />
+      <div className="Bank-details">
+        <span className="bankname">{Bank_name}</span><br />
+          <Link to={`/bank/${Bank_id}/branches`} className="branchBtn">
+            <button>See Branches</button>
+          </Link>
         </div>
-    );
+    </div>
+  );
 }
-
 
 export default Bank;
---
+++ client/src/International.js
@@ -0,0 +1,67 @@
+import React from "react";
+import axios from "axios";
+import Bank from "./Bank";
+import Loading from "./Loading";
+import Error from "./Error";
+
+class BankList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bankDisplay: [],
+            loading: false,
+            error: true
+        };
+    }
+
+    componentDidMount() {
+        this.fetchbankDisplay();
+    }
+
+    fetchbankDisplay() {
+        this.setState({
+            loading: true,
+            error: false
+        });
+        axios
+            .get("/api/category/international")
+            .then(response => {
+                this.setState({
+                    bankDisplay: response.data,
+                    loading: false,
+                    error: false
+                });
+            })
+            .catch(error => {
+                this.setState({
+                    bankDisplay: [],
+                    loading: false,
+                    error: true
+                });
+            });
+    }
+
+    render() {
+        const { bankDisplay, loading, error } = this.state;
+
+        if (loading) {
+            return <Loading />;
+        }
+
+        if (error) {
+            return <Error />;
+        }
+
+        return (
+            <div className="Bank-container">
+                <div>
+                    {bankDisplay.map(b => (
+                        <Bank key={b.Bank_id} bankDisplay={b} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BankList;
