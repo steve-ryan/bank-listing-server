150d4bafc368fd4b004f55197a78314daf4e2983
stephen wachira <stevohyoung@gmail.com>
2019-08-05 18:57:43 +0300

    fixed issues on Branch

--- client/src/Branch.js
+++ client/src/Branch.js
@@ -1,14 +1,16 @@
 import React from "react";
 function Branch({ branchDisplay }) {
-  const {Logo_url, Branch_name, Phone_Number,Email,Operational_hr  } = branchDisplay;
+  const {Logo_url, Branch_name, Location,Phone_Number,Email,Operation_hrs  } = branchDisplay;
   return (
     <div className="bank-container">
       <img className="banklogo" src={Logo_url} alt={Branch_name} />
       <div className="Bank-details">
         <p className="bankname">{Branch_name}</p><br />
+        <p>{Location}</p>
+        <p>{Operation_hrs}</p>
         <p>{Phone_Number}</p><br />
         <p>{Email}</p><br />
-        <p>{Operational_hr}</p>
+        
         </div>
     </div>
   );
